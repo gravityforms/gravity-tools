4bd25a1f9ec2d42a8ff10f703b1e27c9ad67f73f
Aaron Speer <aaron@rocketgenius.com>
2024-01-31 16:18:51 -0600

    Add in telemetry module files

---
+++ src/Logging/class-logger.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Gravity_Forms\Gravity_Tools\Logging;
+
+class Logger {
+
+	public function log_debug( $message ) {
+		return;
+	}
+
+	public function log_error( $message ) {
+		return;
+	}
+
+}
