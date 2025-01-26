7fc98cf0578a586b2fc027dc0641589882c9e45f
Aaron Speer <aaron@rocketgenius.com>
2025-01-26 09:09:00 -0600

    Docblock utils

--- src/Hermes/Utils/class-model-collection.php
+++ src/Hermes/Utils/class-model-collection.php
@@ -4,6 +4,11 @@ namespace Gravity_Forms\Gravity_Tools\Hermes\Utils;
 
 use Gravity_Forms\Gravity_Tools\Hermes\Models\Model;
 
+/**
+ * Model Collection
+ *
+ * A simple collection class for storing registered Models in the system.
+ */
 class Model_Collection {
 
 	/**
@@ -11,18 +16,48 @@ class Model_Collection {
 	 */
 	protected $models = array();
 
+	/**
+	 * Register a model type.
+	 *
+	 * @param string $type
+	 * @param Model  $model
+	 *
+	 * @return void
+	 */
 	public function add( $type, Model $model ) {
 		$this->models[ $type ] = $model;
 	}
 
+
+	/**
+	 * Unregister/remove a Model type.
+	 *
+	 * @param string $type
+	 *
+	 * @return void
+	 */
 	public function remove( $type ) {
 		unset( $this->models[ $type ] );
 	}
 
+	/**
+	 * Check if a Model of a given type exists in the collection.
+	 *
+	 * @param string $type
+	 *
+	 * @return bool
+	 */
 	public function has( $type ) {
 		return array_key_exists( $type, $this->models );
 	}
 
+	/**
+	 * Get a Model from the collection by its type.
+	 *
+	 * @param string $type
+	 *
+	 * @return Model|null
+	 */
 	public function get( $type ) {
 		if ( ! $this->has( $type ) ) {
 			return null;
--- src/Hermes/Utils/class-relationship-collection.php
+++ src/Hermes/Utils/class-relationship-collection.php
@@ -2,6 +2,11 @@
 
 namespace Gravity_Forms\Gravity_Tools\Hermes\Utils;
 
+/**
+ * Relationshp Collection
+ *
+ * A simple collection handling Relationship objects.
+ */
 class Relationship_Collection {
 
 	/**
@@ -12,28 +17,49 @@ class Relationship_Collection {
 	/**
 	 * @param Relationship[] $relationships
 	 */
-	public function __construct( $relationships ) {
+	public function __construct( $relationships = array() ) {
 		$this->relationships = $relationships;
 	}
 
+	/**
+	 * Add a Relationship to the collection.
+	 *
+	 * @param Relationship $relationship
+	 *
+	 * @return void
+	 */
 	public function add( Relationship $relationship ) {
 		$this->relationships[] = $relationship;
 	}
 
+	/**
+	 * Get a given Relationship by the related object.
+	 *
+	 * @param string $related_object
+	 *
+	 * @return Relationship|mixed|null
+	 */
 	public function get( $related_object ) {
 		if ( ! $this->has( $related_object ) ) {
 			return null;
 		}
 
-		$relationship = array_filter( $this->relationships, function( $relationship ) use ( $related_object ) {
+		$relationship = array_filter( $this->relationships, function ( $relationship ) use ( $related_object ) {
 			return $relationship->to() === $related_object;
 		} );
 
 		return array_shift( $relationship );
 	}
 
+	/**
+	 * Check if the collection contains a relationship for the given object type.
+	 *
+	 * @param string $related_object
+	 *
+	 * @return bool
+	 */
 	public function has( $related_object ) {
-		$relationship = array_filter( $this->relationships, function( $relationship ) use ( $related_object ) {
+		$relationship = array_filter( $this->relationships, function ( $relationship ) use ( $related_object ) {
 			return $relationship->to() === $related_object;
 		} );
 
