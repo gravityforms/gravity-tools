e6967c76cd13f2267bdaa0cc81454fa2f5360ed8
Aaron Speer <aaron@rocketgenius.com>
2025-01-26 09:38:24 -0600

    Docblock Mutation handler

--- src/Hermes/class-mutation-handler.php
+++ src/Hermes/class-mutation-handler.php
@@ -7,54 +7,78 @@ use Gravity_Forms\Gravity_Tools\Hermes\Models\Model;
 use Gravity_Forms\Gravity_Tools\Hermes\Runners\Connect_Runner;
 use Gravity_Forms\Gravity_Tools\Hermes\Runners\Delete_Runner;
 use Gravity_Forms\Gravity_Tools\Hermes\Runners\Insert_Runner;
+use Gravity_Forms\Gravity_Tools\Hermes\Runners\Runner;
 use Gravity_Forms\Gravity_Tools\Hermes\Runners\Update_Runner;
-use Gravity_Forms\Gravity_Tools\Hermes\Tokens\Data_Object_From_Array_Token;
-use Gravity_Forms\Gravity_Tools\Hermes\Tokens\Field_Token;
-use Gravity_Forms\Gravity_Tools\Hermes\Tokens\Mutations\Connect\Connect_Mutation_Token;
-use Gravity_Forms\Gravity_Tools\Hermes\Tokens\Mutations\Delete\Delete_Mutation_Token;
-use Gravity_Forms\Gravity_Tools\Hermes\Tokens\Mutations\Generic_Mutation_Token;
-use Gravity_Forms\Gravity_Tools\Hermes\Tokens\Mutations\Insert\Insert_Mutation_Token;
-use Gravity_Forms\Gravity_Tools\Hermes\Tokens\Mutations\Update\Update_Mutation_Token;
 use Gravity_Forms\Gravity_Tools\Hermes\Tokens\Query_Token;
 use Gravity_Forms\Gravity_Tools\Hermes\Utils\Model_Collection;
 
+/**
+ * The initial entry point for handling Mutation requests. For Query handling, see Query_Handler.
+ */
 class Mutation_Handler {
 
 	/**
+	 * The namespace to use when querying DB tables. The namespace is used after the $wpdb->prefix
+	 * value and before the actual table name.
+	 *
+	 * Example:
+	 *
+	 * Passing `gravitytools` would result in a meta table name of `wp_gravitytools_meta`..
+	 *
 	 * @var string
 	 */
 	protected $db_namespace;
 
 	/**
+	 * The collection of models supported for queries.
+	 *
 	 * @var Model_Collection
 	 */
 	protected $models;
 
 	/**
+	 * A valid Query Handler for retrieving the resulting objects after an insert/update.
+	 *
 	 * @var Query_Handler
 	 */
 	protected $query_handler;
 
 	/**
+	 * The Runner for handling Insert mutations.
+	 *
 	 * @var Insert_Runner
 	 */
 	protected $insert_runner;
 
 	/**
+	 * The Runner for handling Delete mutations.
+	 *
 	 * @var Delete_Runner
 	 */
 	protected $delete_runner;
 
 	/**
+	 * The Runner for handling Update mutations.
+	 *
 	 * @var Update_Runner
 	 */
 	protected $update_runner;
 
 	/**
+	 * The Runner for haldling Connect mutations.
+	 *
 	 * @var Connect_Runner
 	 */
 	protected $connect_runner;
 
+	/**
+	 * Constructor
+	 *
+	 * @param string           $db_namespace
+	 * @param Model_Collection $models
+	 * @param Query_Handler    $query_handler
+	 * @param Runner[]         $runners
+	 */
 	public function __construct( $db_namespace, $models, $query_handler, $runners ) {
 		$this->db_namespace   = $db_namespace;
 		$this->models         = $models;
@@ -65,9 +89,19 @@ class Mutation_Handler {
 		$this->connect_runner = $runners['connect'];
 	}
 
+	/**
+	 * Parse the provided Mutation string and execute the appropriate SQL queries to perform the
+	 * mutation.
+	 *
+	 * @param string $mutation_string
+	 *
+	 * @return void
+	 */
 	public function handle_mutation( $mutation_string ) {
 		global $wpdb;
 
+
+		// Pass the string to the Generic Mutation token to determine the specific mutation type.
 		$generic_mutation = new Generic_Mutation_Token( $mutation_string );
 
 		/**
@@ -75,6 +109,7 @@ class Mutation_Handler {
 		 */
 		$mutation = $generic_mutation->mutation();
 
+		// Ensure the object type in question is registered in our Model Collection.
 		if ( ! $this->models->has( $mutation->object_type() ) ) {
 			$error_message = sprintf( 'Mutation attempted with invalid object type: %s', $mutation->object_type() );
 			throw new \InvalidArgumentException( $error_message );
@@ -82,11 +117,13 @@ class Mutation_Handler {
 
 		$object_model = $this->models->get( $mutation->object_type() );
 
+		// Ensure the querying user has the appropriate permissions to access data for this object.
 		if ( ! $object_model->has_access() ) {
 			$error_message = sprintf( 'Access not allowed for object type %s', $mutation->object_type() );
 			throw new \InvalidArgumentException( $error_message );
 		}
 
+		// Handle the actual mutation based on the identified mutation type by calling its appropriate Runner.
 		switch ( $mutation->operation() ) {
 			case 'insert':
 				$this->insert_runner->run( $mutation, $object_model );
@@ -103,16 +140,6 @@ class Mutation_Handler {
 			default:
 				break;
 		}
-	}
-
-	/**
-	 * @param Connect_Mutation_Token $mutation
-	 * @param Model                  $object_model
-	 *
-	 * @return void
-	 */
-	private function handle_connect_mutation( $mutation, $object_model ) {
-
 	}
 
 }
