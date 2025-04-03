2c669609e25e534343551c6b718ed5a7032a4481
Aaron Speer <aaron@rocketgenius.com>
2025-04-03 11:18:41 -0500

    Fix connections for sub-sub-children

--- src/Hermes/Runners/class-insert-runner.php
+++ src/Hermes/Runners/class-insert-runner.php
@@ -55,13 +55,25 @@ class Insert_Runner extends Runner {
 			$categorized_fields['local']['dateUpdated'] = gmdate( 'Y-m-d H:i:s', time() );
 
 			$inserted_id = $this->handle_single_insert( $object_model, $categorized_fields );
-
 			if ( $object->is_child() && ( empty( $child_ids ) || array_key_first( $child_ids ) === $object->object_type() ) ) {
 
 				if ( ! isset( $child_ids[ $object->object_type() ] ) ) {
 					$child_ids[ $object->object_type() ] = array();
 				}
+				$child_ids[ $object->object_type() ][] = $inserted_id;
+				continue;
+			}
 
+			if ( $object->is_child() && ! empty( $child_ids ) ) {
+				foreach ( $child_ids as $child_type => $children ) {
+					foreach ( $children as $child_id ) {
+						$this->connect_runner->run_single( $object->object_type(), $child_type, $inserted_id, $child_id, $object_model );
+					}
+				}
+
+				$child_ids = array(
+					$object->object_type() => array(),
+				);
 				$child_ids[ $object->object_type() ][] = $inserted_id;
 				continue;
 			}
@@ -75,7 +87,6 @@ class Insert_Runner extends Runner {
 
 				$child_ids = array();
 			}
-
 			$inserted_ids[] = $inserted_id;
 		}
 
