0851beec5e49a7fd2989cce4e52372123682ed69
speerface <aaron@rocketgenius.com>
2025-09-08 11:11:26 -0500

    Formatting

--- src/Utils/class-beltloop.php
+++ src/Utils/class-beltloop.php
@@ -4,11 +4,10 @@ namespace Gravity_Forms\Gravity_Tools\Utils;
 
 class Beltloop {
 
-
 	public static function sort( $data, $id_key = 'id', $sort_key = 'prevId' ) {
 		$indexed = array();
 
-		foreach( $data as $datum ) {
+		foreach ( $data as $datum ) {
 			$indexed[ $datum[ $sort_key ] ] = $datum;
 		}
 
@@ -20,12 +19,14 @@ class Beltloop {
 			$current_index = array_key_first( $indexed );
 		}
 
-		while( count( $indexed ) ) {
+		while ( count( $indexed ) ) {
 			if ( ! array_key_exists( $current_index, $indexed ) ) {
 				$current_index = array_key_first( $indexed );
 			}
-			$current = $indexed[ $current_index ];
+
+			$current  = $indexed[ $current_index ];
 			$sorted[] = $current;
+
 			unset( $indexed[ $current_index ] );
 			$current_index = $current[ $id_key ];
 		}
@@ -36,15 +37,15 @@ class Beltloop {
 	public static function partial_sort( $full_list, $partial_list, $id_key = 'id' ) {
 		$indexed = array();
 
-		foreach( $full_list as $item ) {
+		foreach ( $full_list as $item ) {
 			$indexed[ $item[ $id_key ] ] = $item;
 		}
 
 		$sorted = array();
 
-		foreach( $partial_list as $part_item ) {
-			$search = $part_item[ $id_key ];
-			$pos = (int) array_search( $search, array_keys( $indexed ) );
+		foreach ( $partial_list as $part_item ) {
+			$search         = $part_item[ $id_key ];
+			$pos            = (int) array_search( $search, array_keys( $indexed ) );
 			$sorted[ $pos ] = $part_item;
 		}
 
